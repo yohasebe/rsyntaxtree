b311f70ad4aa16a90c9e487eec4d6b4df8b63a0b
Yoichiro Hasebe <yohasebe@gmail.com>
2013-05-11 21:18:45 +0900

    Web readme modified

--- lib/rsyntaxtree.rb
+++ lib/rsyntaxtree.rb
@@ -68,6 +68,10 @@ class RSGenerator
           new_params["font"] = FONT_DIR + "/ipamp.ttf"
         elsif value == "cjk"
           new_params["font"] = FONT_DIR + "/wqy-zenhei.ttf"
+        elsif value == "aru"
+          new_params["font"] = FONT_DIR + "/ArialUnicode.ttf"
+        elsif value == "tnr"
+          new_params["font"] = FONT_DIR + "/TimesNewRoman.ttf"
         end
       else
         new_params[key] = value
