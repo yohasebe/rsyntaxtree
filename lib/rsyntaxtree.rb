ecf5f275d65adbba971ff1e75c52e9d82c377c24
Yoichiro Hasebe <yohasebe@gmail.com>
2021-02-17 14:50:51 +0900

    Fix for Ruby 3.0.0

--- lib/rsyntaxtree.rb
+++ lib/rsyntaxtree.rb
@@ -10,7 +10,7 @@
 #
 # This file is part of RSyntaxTree, which is a ruby port of Andre Eisenbach's
 # excellent program phpSyntaxTree.
-# Copyright (c) 2007-2018 Yoichiro Hasebe <yohasebe@gmail.com>
+# Copyright (c) 2007-2021 Yoichiro Hasebe <yohasebe@gmail.com>
 # Copyright (c) 2003-2004 Andre Eisenbach <andre@ironcreek.net>
 
 $LOAD_PATH << File.join( File.dirname(__FILE__), 'rsyntaxtree')
@@ -39,7 +39,7 @@ class RSGenerator
       key = keystr.to_sym
       case key
       when :data
-        data = URI.unescape(value)
+        data = URI.decode_www_from(value)
         data  = data.gsub('-AMP-', '&')
           .gsub('-PERCENT-', "%")
           .gsub('-PRIME-', "'")
--- lib/rsyntaxtree/error_message.rb
+++ lib/rsyntaxtree/error_message.rb
@@ -10,7 +10,7 @@
 # This file is part of RSyntaxTree, which is a ruby port of Andre Eisenbach's
 # excellent program phpSyntaxTree.
 #
-# Copyright (c) 2007-2018 Yoichiro Hasebe <yohasebe@gmail.com>
+# Copyright (c) 2007-2021 Yoichiro Hasebe <yohasebe@gmail.com>
 # Copyright (c) 2003-2004 Andre Eisenbach <andre@ironcreek.net>
 
 class ErrorMessage
